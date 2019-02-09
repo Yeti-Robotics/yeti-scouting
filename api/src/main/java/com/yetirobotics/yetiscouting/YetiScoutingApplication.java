22ee39a50a3c616986b8c278e40d1feeb93e613c
Antoine Campbell <acampbell@captechventures.com>
2019-02-09 10:49:50 -0500

    - Initial commit

---
+++ api/src/main/java/com/yetirobotics/yetiscouting/YetiScoutingApplication.java
@@ -0,0 +1,14 @@
+package com.yetirobotics.yetiscouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetiScoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetiScoutingApplication.class, args);
+	}
+
+}
+
