b15765c7e0538d145ec39e52bf9941577727d6d2
Valerie <valukutty.val@gmail.com>
2019-02-09 13:52:27 -0500

    updated something related to the website

--- scouting-web/src/app/app.component.ts
+++ scouting-web/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
+  teams: any[];
+  constructor(private httpClient:HttpClient) {
+    
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get("http://localhost:8080/teams")
+    .subscribe(data => {
+      this.teams = data['_embedded']['teams'];
+    });
+  }
 }
