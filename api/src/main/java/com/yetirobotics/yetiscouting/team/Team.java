31ce76c5bf175b027602d66575622b41a81d1d03
Sam Perlmutter <sam.perlmutter227@gmail.com>
2019-03-19 15:30:40 -0400

    Converted schema and queries to using mysql managed by docker

--- api/src/main/java/com/yetirobotics/yetiscouting/team/Team.java
+++ api/src/main/java/com/yetirobotics/yetiscouting/team/Team.java
@@ -1,10 +1,10 @@
 package com.yetirobotics.yetiscouting.team;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.ColumnResult;
 import javax.persistence.ConstructorResult;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.NamedNativeQuery;
 import javax.persistence.SqlResultSetMapping;
 import javax.persistence.Table;
@@ -21,12 +21,12 @@ import lombok.Data;
     name = "Team.teamList",
     query = "SELECT s.team_number AS teamNumber,"+
    "t.team_name AS teamName,"+
-   "AVG(CAST (s.sandstorm_cargo_hatch_panel_count AS DOUBLE) + CAST (s.sandstorm_rocket_hatch_panel_count AS DOUBLE)) AS avgDisk,"+
-   "AVG(CAST (s.sandstorm_cargo_ball_count AS DOUBLE) + CAST (s.sandstorm_rocket_ball_count AS DOUBLE)) AS avgBall,"+
-   "SUM(S.DROPPED_GAME_PIECES) AS droppedGamePieces,"+
-   "MAX(s.HAB_LEVEL_CLIMB) AS maxLevelClimbed,"+
-   "AVG(CAST(s.DEFENSE AS DOUBLE)) AS avgDefensePlays "+
-   "FROM SCOUTING_FORM s "+
+   "AVG(s.sandstorm_cargo_hatch_panel_count + s.sandstorm_rocket_hatch_panel_count) AS avgDisk,"+
+   "AVG(s.sandstorm_cargo_ball_count + s.sandstorm_rocket_ball_count) AS avgBall,"+
+   "SUM(s.dropped_game_pieces) AS droppedGamePieces,"+
+   "MAX(s.hab_level_climb) AS maxLevelClimbed,"+
+   "AVG(s.defense) AS avgDefensePlays "+
+   "FROM scouting_form s "+
    "LEFT JOIN team t ON t.team_number = s.team_number "+
    "GROUP BY s.team_number",
     resultClass = TeamList.class,
@@ -37,25 +37,25 @@ import lombok.Data;
     name = "Team.teamStats",
     query =  "   SELECT s.team_number AS teamNumber,  "  + 
     "   t.team_name AS teamName,  "  + 
-    "   AVG(CAST (s.sandstorm_cargo_hatch_panel_count AS DOUBLE)) AS avgSandCargoDisk,  "  + 
-    "   AVG(CAST (s.sandstorm_rocket_hatch_panel_count AS DOUBLE)) AS avgSandRocketDisk,  "  + 
-    "   AVG(CAST (s.sandstorm_cargo_ball_count AS DOUBLE)) AS avgSandCargoBall,  "  + 
-    "   AVG(CAST (s.sandstorm_rocket_ball_count AS DOUBLE)) AS avgSandRocketBall,  "  + 
-    "   AVG(CAST(s.cross_habitat_line AS DOUBLE)) * 100 AS percentSandCrossLine,  "  + 
-    "   AVG(CAST (S.DROPPED_GAME_PIECES AS DOUBLE)) AS avgDroppedTeleGamePieces,  "  + 
-    "   MAX(s.HAB_LEVEL_CLIMB) AS maxTeleLevelClimbed,  "  + 
-    "   AVG(CAST(s.DEFENSE AS DOUBLE)) * 100 AS avgTeleDefensePlays,  "  + 
+    "   AVG(s.sandstorm_cargo_hatch_panel_count) AS avgSandCargoDisk,  "  +
+    "   AVG(s.sandstorm_rocket_hatch_panel_count) AS avgSandRocketDisk,  "  +
+    "   AVG(s.sandstorm_cargo_ball_count) AS avgSandCargoBall,  "  +
+    "   AVG(s.sandstorm_rocket_ball_count) AS avgSandRocketBall,  "  +
+    "   AVG(s.cross_habitat_line) * 100 AS percentSandCrossLine,  "  +
+    "   AVG(s.dropped_game_pieces) AS avgDroppedTeleGamePieces,  "  +
+    "   MAX(s.hab_level_climb) AS maxTeleLevelClimbed,  "  +
+    "   AVG(s.defense) * 100 AS avgTeleDefensePlays,  "  +
     "   (SELECT hab_level_start  "  + 
     "   FROM scouting_form  "  + 
-    "   WHERE team_number=:teamNumber  "  + 
+    "   WHERE team_number=:teamNumber  "  +
     "   GROUP BY hab_level_start  "  + 
     "   ORDER BY COUNT(hab_level_start) DESC, hab_level_start DESC  "  + 
     "   LIMIT 1) AS mostCommonSandHabLevelStart,  "  + 
-    "   AVG(CAST (s.teleop_cargo_hatch_panel_count AS DOUBLE)) AS avgTeleCargoDisk,  "  + 
-    "   AVG(CAST (s.teleop_rocket_hatch_panel_count AS DOUBLE)) AS avgTeleRocketDisk,  "  + 
-    "   AVG(CAST (s.teleop_cargo_ball_count AS DOUBLE)) AS avgTeleCargoBall,  "  + 
-    "   AVG(CAST (s.teleop_rocket_ball_count AS DOUBLE)) AS avgTeleRocketBall  "  + 
-    "   FROM SCOUTING_FORM s  "  + 
+    "   AVG(s.teleop_cargo_hatch_panel_count) AS avgTeleCargoDisk,  "  +
+    "   AVG(s.teleop_rocket_hatch_panel_count) AS avgTeleRocketDisk,  "  +
+    "   AVG(s.teleop_cargo_ball_count) AS avgTeleCargoBall,  "  +
+    "   AVG(s.teleop_rocket_ball_count) AS avgTeleRocketBall  "  +
+    "   FROM scouting_form s  "  +
     "   LEFT JOIN team t ON t.team_number = s.team_number  "  + 
     "   WHERE s.team_number = :teamNumber  "  + 
     "  GROUP BY s.team_number  ",
