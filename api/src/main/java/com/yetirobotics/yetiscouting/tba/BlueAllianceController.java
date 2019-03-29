e26ce45bcadb63d2bfd7d0cf3b43aed7b2660d30
Sam Perlmutter <sam.perlmutter227@gmail.com>
2019-03-29 11:15:16 -0400

    - Match schedule now only calls blue alliance if there isn't already a schedule in memory - Moved schedule api call to its own method for future use

--- api/src/main/java/com/yetirobotics/yetiscouting/tba/BlueAllianceController.java
+++ api/src/main/java/com/yetirobotics/yetiscouting/tba/BlueAllianceController.java
@@ -11,10 +11,7 @@ import com.yetirobotics.yetiscouting.form.ScoutingFormRepository;
 import com.yetirobotics.yetiscouting.tba.match.Match;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +24,7 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/tba")
 public class BlueAllianceController {
     private ScoutingFormRepository scoutingFormRepository;
+    private List<Match> schedule = new ArrayList<>();
 
     @Autowired
     public BlueAllianceController(ScoutingFormRepository scoutingFormRepository) {
@@ -37,17 +35,9 @@ public class BlueAllianceController {
     public ResponseEntity getFutureMatches() {
         int lastMatch = scoutingFormRepository.findTopByOrderByMatchNumberDesc().getMatchNumber();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-TBA-Auth-Key", "");
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> result = restTemplate.exchange(
-                "https://www.thebluealliance.com/api/v3/event/2019ncgui/matches", HttpMethod.GET, entity, String.class);
-
         List<Match> matches, futureMatches;
         try {
-            matches = new ObjectMapper().readValue(result.getBody(), new TypeReference<List<Match>>() {});
+            matches = !schedule.isEmpty() ? schedule : new ObjectMapper().readValue(updateMatchSchedule().getBody(), new TypeReference<List<Match>>() {});
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.status(500).build();
@@ -62,7 +52,18 @@ public class BlueAllianceController {
 
         futureMatches.sort(Comparator.comparing(Match::getMatchNumber));
 
-        return ResponseEntity.ok().body(futureMatches);
+        return ResponseEntity.ok(futureMatches);
+    }
+
+    @RequestMapping(value = "/updateSchedule", method = RequestMethod.GET)
+    public ResponseEntity<String> updateMatchSchedule() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-TBA-Auth-Key", "ChjNxHnTP0XQSHsn7xqjc7iTmWHqInOmxNwGfXaWFTA2c3vptfQUQYAORb5dpsNY");
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        ResponseEntity<String> result = new RestTemplate().exchange(
+            "https://www.thebluealliance.com/api/v3/event/2019ncgui/matches", HttpMethod.GET, entity, String.class);
+
+        return ResponseEntity.ok(result.getBody());
     }
 
 }
