d6c8650d5afc26307698470a9f2ac5fd9fe29816
Antoine Campbell <acampbell@captechventures.com>
2020-02-23 12:38:59 -0500

    - Testing deployment

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-./gradlew clean assemble
-
-gcloud compute scp api/build/libs/api*.jar scouting:/scouting/api.jar
-gcloud compute scp archive.zip scouting:/scouting/archive.zip
-
-gcloud compute ssh scouting --command "sudo systemctl restart api"
